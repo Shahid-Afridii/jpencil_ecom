36d9cb214a5d0128281099f951aecbf6f0f8ba71
Shahid afridi <shahidafridics@gmail.com>
2023-01-25 18:18:09 +0530

    user page commits

--- pages/Address.jsx
+++ pages/Address.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import Image from "next/image"; 
 import Link from "next/link";
 import user from "../public/images/user.jpeg"
-const User = () => {
+const Address = () => {
   return (
     <>
-       {/* Start Breadcrumbs */}
-       <div className='container py-4 flex justify-between '>
+     {/* Start Breadcrumbs */}
+     <div className='container py-4 flex justify-between '>
                             <div className='flex gap-3 items-center'>
                                       <Link href="/" className='text-primary text-base'>
                                               <i className='fas fa-home'></i>
@@ -22,10 +22,7 @@ const User = () => {
                             </div>
                 </div>
             {/* End Breadcrumbs */}
-
-            {/*Start Account Wrapper  */}
-
-                    <div className="container lg:grid grid-cols-12 items-start gap-6 pt-4 pb-16">
+  <div className="container lg:grid grid-cols-12 items-start gap-6 pt-4 pb-16">
                                 {/* Start Sidebar  */}
                                 <div className="col-span-3">
                                     {/* Start User Profile */}
@@ -45,17 +42,17 @@ const User = () => {
                                       <div className='mt-6 bg-gray-100 shadow rounded p-4 divide-y divide-gray-200 space-y-4 text-gray-600'> 
                                         {/* Start Single Link */}
                                         <div className="space-y-1 pl-8">
-                                                <Link href="#" className='relative text-base font-medium capitalize hover:text-primary transition block text-primary'>
+                                                <Link href="/User" className='relative text-base font-medium capitalize hover:text-primary transition block'>
                                                   Manage Account
                                                   <span className="absolute -left-8 top-0 text-base">
                                                     <i className='far fa-address-card'></i>
                                                   </span>
                                                 </Link>
-                                             
-                                                <Link href="/Address" className='hover:text-primary transition capitalize block'>
+                                              
+                                                <Link href="#" className='hover:text-primary transition capitalize block  text-primary'>
                                                  Manage Address
                                                 </Link>
-                                                <Link href="/ChangeAddress" className='hover:text-primary transition capitalize block'>
+                                                <Link href="/ChangePassword" className='hover:text-primary transition capitalize block'>
                                                   Change Password
                                                 </Link>
                                         </div>
@@ -118,56 +115,70 @@ const User = () => {
                                       </div>
                                     {/* End Profile Link */}
                                 </div>
-                                {/* End Sidebar  */}
+                                 {/* End Sidebar  */}
 
-                                {/* Start  Account content */}
-                                <div className="col-span-9 grid md:grid-cols-3 gap-4 mt-6 lg:mt-0">
-                                  {/* Start Single Card  */}
-                                  <div className="shadow rounded bg-gray-100 px-4 pt-6 pb-8"> 
-                                  <div className="flex justify-between items-center mb-4">
-                                          <h3 className='font-medium capitalize text-gray-800 text-lg'>Personal Profile</h3>
-                                  <Link href="" className='text-primary font-poppins'>Edit</Link>
-                                  </div>
-                                  <div className='space-y-1'>
-                                  <h3 className='text-gray-700 font-semibold font-poppins'>Shahid Afridi</h3>
-                                  <p className='text-gray-800'>shahid@jpencil.com</p>
-                                  <p className='text-gray-800'>(+91) 8056585599</p>
-                                  </div>
-                                  </div>
-                                  {/*End Single Card  */}
-                                  {/* Start Single Card  */}
-                                  <div className="shadow rounded bg-gray-100 px-4 pt-6 pb-8"> 
-                                  <div className="flex justify-between items-center mb-4">
-                                          <h3 className='font-medium capitalize text-gray-800 text-lg'>Shipping Address</h3>
-                                  <Link href="" className='text-primary font-poppins'>Edit</Link>
-                                  </div>
-                                  <div className='space-y-1'>
-                                  {/* <h3 className='text-gray-700 font-semibold font-poppins'>Shahid Afridi</h3> */}
-                                  <p className='text-gray-800'>Arunachala Towers, D.No. 6 3rd Floor, Nanjundapuram Rd, Nethaji Nagar, Ramanathapuram, Tamil Nadu 641036</p>
-                                
-                                  </div>
-                                  </div>
-                                  {/*End Single Card  */}
-                                  {/* Start Single Card  */}
-                                  <div className="shadow rounded bg-gray-100 px-4 pt-6 pb-8"> 
-                                  <div className="flex justify-between items-center mb-4">
-                                          <h3 className='font-medium capitalize text-gray-800 text-lg'>Billing Address</h3>
-                                  <Link href="" className='text-primary font-poppins'>Edit</Link>
-                                  </div>
-                                  <div className='space-y-1'>
-                                  {/* <h3 className='text-gray-700 font-semibold font-poppins'>Shahid Afridi</h3> */}
-                                  <p className='text-gray-800'>Arunachala Towers, D.No. 6 3rd Floor, Nanjundapuram Rd, Nethaji Nagar, Ramanathapuram, Tamil Nadu 641036</p>
-                                 
-                                  </div>
-                                  </div>
-                                  {/*End Single Card  */}
+                                 {/* Start Manage Address */}
+                                 <div className="col-span-9 shadow rounded px-6 pt-5 pb-7 bg-gray-100 mt-6 lg:mt-0"> 
+                                        <form method="" action="">
+                                            <h3 className='text-lg font-medium capitalize mb-4'>Manage Address</h3>
+                                            <div className='space-y-4 '>
+                                                <div className='grid sm:grid-cols-2 gap-4'>
+                                                        {/*Start Single Input */}
+                                                        <div>
+                                                            <label htmlFor="" className='text-gray-600 mb-2 block'>Full Name</label>
+                                                            <input type="text" className='input-box'placeholder='User Info' />
+                                                        </div>
+                                                        {/*End Single Input */}
+                                                        {/*Start Single Input */}
+                                                        <div>
+                                                            <label htmlFor="" className='text-gray-600 mb-2 block'>Phone Number</label>
+                                                            <input type="text" className='input-box'placeholder='(+91) 90xxxxxxxx' />
+                                                        </div>
+                                                        {/*End Single Input */}
+                                                </div>
+                                                <div className='grid sm:grid-cols-2 gap-4'>
+                                                      <div>
+                                                        <label htmlFor="" className='text-gray-600 mb-2 block'>Country</label>
+                                                        <select  className='input-box'>
+                                                            <option value="India">India</option>
+                                                            <option value="America">America</option>
+                                                            <option value="Canada">Canada</option>
+                                                        </select>
+                                                      </div>
+                                                      <div>
+                                                        <label htmlFor="" className='text-gray-600 mb-2 block'>State</label>
+                                                        <select  className='input-box'>
+                                                            <option value="TamilNadu">TamilNadu</option>
+                                                            <option value="Kerala">Kerala</option>
+                                                            <option value="Bangalore">Bangalore</option>
+                                                        </select>
+                                                      </div>
+                                                      <div>
+                                                        <label htmlFor="" className='text-gray-600 mb-2 block'>City</label>
+                                                        <input type="text" className='input-box'placeholder='User City' />
+                                                      </div>
+                                                      <div>
+                                                        <label htmlFor="" className='text-gray-600 mb-2 block'>Area</label>
+                                                        <input type="text" className='input-box'placeholder='User Area' />
+                                                      </div>
+                                                </div>
+
+                                                            <div>
+                                                            <label htmlFor="" className='text-gray-600 mb-2 block'>Address</label>
+                                                            <input type="text" className='input-box'placeholder='User Address' />
+                                                            </div>
+
+                                                            <div className='mt-6 '>
+                                                            <button type="submit" className='mt-4 px-6 py-2 text-center text-white bg-primary border hover:font-semibold border-primary rounded
+                                                            hover:bg-transparent hover:text-primary transition uppercase font-poppins font-medium  '>Save Changes</button>
+                                                            </div>
+                                            </div>
+                                        </form>
+                                 </div>
+                                  {/* End Manage Address */}
                                 </div>
-                                {/* End  Account content */}
-                    </div>
-
-            {/*End Account Wrapper  */}
     </>
   )
 }
 
-export default User
+export default Address
